7139faf022223196e192aa667cb510975f586c05
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-03 16:28:19 +0530

    Remove primary key generation

--- Database/business_table.py
+++ Database/business_table.py
@@ -9,18 +9,15 @@ agentstatus = ''
 connection = mdb.connect(host='localhost',user='root',password='',database='prism_ai_database')
 cont = connection.cursor() # controller to control the database
 
-def addRow(name, contact, type):
+def addRow(name, contact, password, type):
     '''function which can be used to add rows 
     to the business data table in the database'''
-    global id
 
-    cont.execute('SELECT businessId FROM business')
-    id = random.randrange(1000000, 9999999)
-    while id in cont.fetchall(): id = random.randrange(1000000, 9999999) # creates unique ID for business
+    agentStatus = 0 # to be changed later
 
-    agentStatus = 0 #to be changed later
-
-    cont.execute(f"INSERT INTO business VALUES ({id},'{name}','{contact}','{type}',{agentStatus})")
+    cont.execute(f"INSERT INTO business (Name, Contact, Password, Type, Agent_Status) VALUES ('{name}','{contact}','{type}','{password}',{agentStatus})")
 
     connection.commit()
     connection.close
+
+addRow('test','0728000031','test','test')
