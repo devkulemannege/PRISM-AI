cdabe6b34d613ca1d607390b8d1632b2c62b455b
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-03 11:06:05 +0530

    Add unique ID generation

--- Database/product_table.py
+++ Database/product_table.py
@@ -1,5 +1,15 @@
 import mariadb as mdb
+import random
 
 connection = mdb.connect(host='localhost',user='root',password='',database='prism_ai_database')
 cont = connection.cursor() # controller to control the database
+
+def addRow():
+    
+    cont.execute('SELECT productId FROM product')
+    id = random.randrange(1000000, 9999999)
+    while id in cont.fetchall(): id = random.randrange(1000000, 9999999) # creates unique ID for business
+
+    connection.commit()
+    connection.close()
 
