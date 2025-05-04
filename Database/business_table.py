049b500951c7dfcbc20752d730f6fca8b27d6caa
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-04 21:35:49 +0530

    Add function variables: "template", "prompt"

--- Database/business_table.py
+++ Database/business_table.py
@@ -8,11 +8,11 @@ agentstatus = ''
 connection = mdb.connect(host='localhost',user='root',password='',database='prism_ai_database')
 cont = connection.cursor() # controller to control the database
 
-def addRow(name, contact, password, type):
+def addRow(name, contact, password, type, template, prompt):
     '''function which can be used to add rows 
     to the business data table in the database'''
     agentStatus = 0 # to be changed later
 
-    cont.execute(f"INSERT INTO business (Name, Contact, `Password`, Type, Agent_Status) VALUES ('{name}','{contact}','{type}','{password}',{agentStatus})")
+    cont.execute(f"INSERT INTO business (name, contact, `password`, type, template, prompt, agentStatus) VALUES ('{name}','{contact}','{type}','{password}','{template}','{prompt}',{agentStatus})")
     connection.commit()
     connection.close
