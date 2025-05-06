53b32184fdde1291e549293f21d3929462f11fa6
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-06 13:59:03 +0530

    Reapply "update system for pastConversation column (does not logically work)"
    This reverts commit b9b5f9e51df73cb3f1ab4c6d9c6f1327e7ad362d.

--- Database/customer_table.py
+++ Database/customer_table.py
@@ -3,6 +3,7 @@ import mariadb as mdb
 # initialization of variables 
 columnLen = 0
 id = 0
+customerId = 0
 
 connection = mdb.connect(host='localhost',user='root',password='',database='prism_ai_database')
 cont = connection.cursor() # controller to control the database
@@ -10,9 +11,25 @@ cont = connection.cursor() # controller to control the database
 def addRow(mobileNo, fName):
     '''function which can be used to add rows
     to the customer data table in the database'''
-    pastConversation = 0 # to be changed
+    global customerId
+    pastConversation = 0
 
-    cont.execute('INSERT INTO customer (Mobile_No, Fname, past_conversation) VALUES (?,?,?)',
+    cont.execute('INSERT INTO customer (mobileNo, fName, pastConversation) VALUES (?,?,?)',
                  (mobileNo, fName, pastConversation))
+    
+    # DISCLAIMER: code below does not logically work.
+    try:
+        cont.execute(f"SELECT customerId FROM customer WEHRE mobileNo = '{mobileNo}'")
+        customerId = cont.fetchall() # fetch customerId corresponding to phone number
+
+        cont.execute(f"SELECT msgId FROM chatlog WHERE customerId = {int(customerId[0][0])}")
+        print(cont.fetchall())
+        cont.execute(f"UPDATE customer SET pastConversation = 1 WEHRE customerId = {int(customerId[0][0])}") # update pastConversation column
+        print(cont.fetchall())
+    except:
+        pastConversation = 0
+
     connection.commit()
     connection.close()
+
+addRow('0728000031','test')
