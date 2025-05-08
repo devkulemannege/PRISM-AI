428155756484f578470051fcf0d805bf4b04e8fe
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-08 13:40:08 +0530

    update import_files folder

---
+++ API Files/import_files/connect_db.py
@@ -0,0 +1,11 @@
+import mariadb as mdb
+
+def connection():
+    '''function which holds the database attributes
+    to establish connection with database'''
+    connection = mdb.connect(host='localhost',user='root',password='',database='prism_ai_database')
+    cont = connection.cursor() # controller to control the database 
+    return connection, cont
+
+
+
