ef4828cd07d67b939fe3bcc4c9010599b3c72c11
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-15 18:59:48 +0530

    Update read_csv.py

--- CSV Reader/read_csv.py
+++ CSV Reader/read_csv.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas 
 import store_headers
 
 def identifyCSV(file):
@@ -12,36 +12,47 @@ def identifyCSV(file):
         'email' : '',
     }
     colHeaderDict = store_headers.colHeaders() # retrive header dictionary from module
-    data = pd.read_csv(file) # access CSV file
+    data = pandas.read_csv(file) # access CSV file
     cell = ''
 
-    for row in len(data):
-        for col in len(data.columns):
+    for row in range(len(data)):
+        for col in range(len(data.columns)):
             cell = data.iloc[row, col] # read every cell in CSV
 
             if data.columns[col] in colHeaderDict['nameHeaders']: # if full name is under one column (SCENARIO 1)
                 # initialize variables for scenario 1
                 tempFirstNameHolder = []
-                tempLastNameHolder = []
-                strNameHolder = ''
+                secondaryFirstNameHolder = []
+                finalValueHolder = ''
                 spaceCount = 0
                 secondarySpaceCount = 0
+                count = 0
 
-                cell = cell.strip() # remove unneccesary whitespaces 
+                cell = cell.strip() # remove front & end whitespaces 
                 for i in cell:
                     if i == ' ':
                         spaceCount += 1
 
-                if spaceCount == 1:
+                if spaceCount == 1: # if 1 whitespace exists, else if multiple exists
                     for i in cell:
-                        tempFirstNameHolder.append(i)
-                else:
+                        count += 1 # keeps track for str indexing
+                        if i == ' ': break
+                        tempFirstNameHolder.append(i) 
+                    finalValueHolder = ''.join(map(str, tempFirstNameHolder)) # converty from list ot str                              
+                else: 
                     for i in cell:
+                        count += 1
                         if i == ' ': secondarySpaceCount = 1
-                        if secondarySpaceCount == spaceCount-1 and spaceCount > 1: break
-                        tempFirstNameHolder.append(i) # add characters 
+                        if secondarySpaceCount == spaceCount: break
+                        tempFirstNameHolder.append(i) 
 
-                strNameHolder = ' '.join(map(str, tempFirstNameHolder))
+                    for i in tempFirstNameHolder:
+                        if i == ' ': break
+                        secondaryFirstNameHolder.append(i)
+                    finalValueHolder = ''.join(map(str, secondaryFirstNameHolder)) # converty from list ot str
+
+                oneInstance['fName'] = finalValueHolder # save first name to dictionary
+                oneInstance['lName'] = cell[count:] # save last name to dictionary
+
 # debug
-identifyCSV(1)
-
+identifyCSV()
