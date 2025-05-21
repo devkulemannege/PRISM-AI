6dcd6de300d7ab6addf182650a81377bb6046f54
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-21 14:30:20 +0530

    Update first, last names assign system

--- CSV Reader/read_csv.py
+++ CSV Reader/read_csv.py
@@ -23,8 +23,6 @@ def identifyCSV(file):
             if data.columns[col] in colHeaderDict['nameHeaders']: # if full name is under one column 
                 # initialize variables for particular scenario
                 tempFirstNameHolder = []
-                secondaryFirstNameHolder = []
-                finalValueHolder = ''
                 spaceCount = 0
                 secondarySpaceCount = 0
                 count = 0
@@ -38,33 +36,22 @@ def identifyCSV(file):
                         count += 1 # keeps track for str indexing
                         if i == ' ': break
                         tempFirstNameHolder.append(i) 
-                    finalValueHolder = ''.join(map(str, tempFirstNameHolder)) # convert from list ot str                              
-                else: 
+                    oneInstance['fName'] = ''.join(map(str, tempFirstNameHolder)) # convert from list ot str and assing to first name
+                    oneInstance['lName'] = cell[count:] # assing last name to last name
+                else:
                     for i in cell:
                         count += 1
-                        if i == ' ': secondarySpaceCount = 1
+                        if i == ' ': secondarySpaceCount += 1
                         if secondarySpaceCount == spaceCount: break
-                        tempFirstNameHolder.append(i) 
+                    oneInstance['fName'] = oneInstance['lName'] =  cell[count:] # assing last name to first name & last name
 
-                    for i in tempFirstNameHolder:
-                        if i == ' ': break
-                        secondaryFirstNameHolder.append(i)
-                    finalValueHolder = ''.join(map(str, secondaryFirstNameHolder)) # converty from list ot str
-
-                oneInstance['fName'] = finalValueHolder # save first name to dictionary
-                oneInstance['lName'] = cell[count:] # save last name to dictionary
-
-            elif data.columns[col] in colHeaderDict['firstNameHeaders']: # if cell contains first name data
-                oneInstance['fName'] = cell
+            elif data.columns[col] in colHeaderDict['firstNameHeaders']: oneInstance['fName'] = cell # if cell contains first name dataoneInstance['fName'] = cell
             
-            elif data.columns[col] in colHeaderDict['lastNameHeaders']: # if cell contains last name data
-                oneInstance['lName'] = cell
+            elif data.columns[col] in colHeaderDict['lastNameHeaders']: oneInstance['lName'] = cell # if cell contains last name data
             
-            elif data.columns[col] in colHeaderDict['emailHeaders']: # if cell contains email data
-                oneInstance['email'] = cell
+            elif data.columns[col] in colHeaderDict['emailHeaders']: oneInstance['email'] = cell # if cell contains email data               
 
-            elif data.columns[col] in colHeaderDict['mobileNumHeaders']: # if cell contains mobile number data
-                oneInstance['mobileNo'] = cell
+            elif data.columns[col] in colHeaderDict['mobileNumHeaders']: oneInstance['mobileNo'] = cell # if cell contains mobile number data         
 
             else: print(f'Column header not identified for cell address {row+1, col+1}')
 
