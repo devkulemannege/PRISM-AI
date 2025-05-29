24a0b802da4b9e01758247b5b9a9e2a6ed41d37b
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2025-05-29 19:16:30 +0530

    Update machine_model.py

--- Online Dashboard/File Reader/File Reader with ML/machine_model.py
+++ Online Dashboard/File Reader/File Reader with ML/machine_model.py
@@ -1,6 +1,5 @@
 import store_headers
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder   
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -17,12 +16,13 @@ for key in colHeaderDict.keys():
 def identify(keyword):
     '''ML based column type identification 
     for a wider range of inupts'''
-    xTrain, xTest, yTrain, yTest = train_test_split(xDataSet, yDataset, test_size=0.1, random_state=1) # split data for training 
+
+    if '_' in keyword: keyword = keyword.replace('_',' ') # removes underscore if found
 
     dataEncode = LabelEncoder() # prepare training for keys (Encoded)
-    yTrainEncode = dataEncode.fit_transform(yTrain)
+    yTrainEncode = dataEncode.fit_transform(yDataset)
     dataVector = TfidfVectorizer() # prepare training for key values (Vectorized )
-    xTrainVector = dataVector.fit_transform(xTrain)
+    xTrainVector = dataVector.fit_transform(xDataSet)
 
     model = LogisticRegression()
     model.fit(xTrainVector, yTrainEncode) # train model (identify correct values for keys [PlaceHolders: value, key])
@@ -30,15 +30,15 @@ def identify(keyword):
     inputDataVec = dataVector.transform([keyword]) # vector conversion 
     predict = model.predict(inputDataVec) # identification using xTrainVector Vector
     confidency = model.predict_proba(inputDataVec)
-    #print(f'Key: {dataEncode.inverse_transform(predict)}') # decode identified key using dataEncode 
         
     if max(confidency[0]) < 0.4: return 'None'
-    else: return dataEncode.inverse_transform(predict)
-
+    else: 
+        print(max(confidency[0]))
+        return dataEncode.inverse_transform(predict)
 
 # debug
 #if str(identify('name')) == "['nameHeaders']": print('YES')
-#print(identify('state'))
+print(identify(input('Enter Keyword: ')))
 
 
 
